9334792b943281227c5ba93f025e3cc2bd681434
i_zvyagin <zvyagin.i.gen@gmail.com>
2025-06-23 00:53:47 +0300

    Git ignore

---
+++ src/test/java/org/app/FirstClassTest.java
@@ -0,0 +1,13 @@
+package org.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirstClassTest {
+
+    @Test
+    public  void test() {
+        assertTrue(true);
+    }
+}
