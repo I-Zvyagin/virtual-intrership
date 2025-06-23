b0e8d6899ec99c33e91fa4f83c5937109013bdc1
i_zvyagin <zvyagin.i.gen@gmail.com>
2025-06-23 14:16:15 +0300

    sout

--- src/main/java/org/app/FirstClass.java
+++ src/main/java/org/app/FirstClass.java
@@ -3,5 +3,6 @@ package org.app;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("I'm learning to commit");
+        System.out.println("I'm learning to commit again");
     }
 }
