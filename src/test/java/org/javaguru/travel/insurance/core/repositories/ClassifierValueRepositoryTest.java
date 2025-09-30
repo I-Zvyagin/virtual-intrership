ad0055f22bd87f3f86471efd1b64a0a1213fa9e3
i_zvyagin <vanyazvyaginfr@gmail.com>
2025-09-30 14:43:34 +0300

    Refactoring ClassifierValueRepositoryTest

--- src/test/java/org/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
+++ src/test/java/org/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
@@ -25,56 +25,32 @@ class ClassifierValueRepositoryTest {
 
     @Test
     public void shouldFind_RiskType_TRAVEL_MEDICAL() {
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                "RISK_TYPE", "TRAVEL_MEDICAL");
-        assertTrue(valueOpt.isPresent());
-        assertEquals("TRAVEL_MEDICAL", valueOpt.get().getIc());
-        assertEquals("RISK_TYPE", valueOpt.get().getClassifier().getTitle());
+        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_MEDICAL");
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_CANCELLATION() {
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                "RISK_TYPE", "TRAVEL_CANCELLATION");
-        assertTrue(valueOpt.isPresent());
-        assertEquals("TRAVEL_CANCELLATION", valueOpt.get().getIc());
-        assertEquals("RISK_TYPE", valueOpt.get().getClassifier().getTitle());
+        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_CANCELLATION");
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_LOSS_BAGGAGE() {
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                "RISK_TYPE", "TRAVEL_LOSS_BAGGAGE");
-        assertTrue(valueOpt.isPresent());
-        assertEquals("TRAVEL_LOSS_BAGGAGE", valueOpt.get().getIc());
-        assertEquals("RISK_TYPE", valueOpt.get().getClassifier().getTitle());
+        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_LOSS_BAGGAGE");
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_THIRD_PARTY_LIABILITY() {
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                "RISK_TYPE", "TRAVEL_THIRD_PARTY_LIABILITY");
-        assertTrue(valueOpt.isPresent());
-        assertEquals("TRAVEL_THIRD_PARTY_LIABILITY", valueOpt.get().getIc());
-        assertEquals("RISK_TYPE", valueOpt.get().getClassifier().getTitle());
+        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_THIRD_PARTY_LIABILITY");
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_EVACUATION() {
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                "RISK_TYPE", "TRAVEL_EVACUATION");
-        assertTrue(valueOpt.isPresent());
-        assertEquals("TRAVEL_EVACUATION", valueOpt.get().getIc());
-        assertEquals("RISK_TYPE", valueOpt.get().getClassifier().getTitle());
+        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_EVACUATION");
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_SPORT_ACTIVITIES() {
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                "RISK_TYPE", "TRAVEL_SPORT_ACTIVITIES");
-        assertTrue(valueOpt.isPresent());
-        assertEquals("TRAVEL_SPORT_ACTIVITIES", valueOpt.get().getIc());
-        assertEquals("RISK_TYPE", valueOpt.get().getClassifier().getTitle());
+        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_SPORT_ACTIVITIES");
     }
 
     @Test
@@ -82,6 +58,13 @@ class ClassifierValueRepositoryTest {
         Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
                 "RISK_TYPE", "FAKE");
         assertTrue(valueOpt.isEmpty());
+    }
+
+    private void getClassifierValueAndAssert(String classifierTitle, String ic) {    Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
+            classifierTitle, ic);
+        assertTrue(valueOpt.isPresent());
+        assertEquals(ic, valueOpt.get().getIc());
+        assertEquals(classifierTitle, valueOpt.get().getClassifier().getTitle());
     }
 
 }
