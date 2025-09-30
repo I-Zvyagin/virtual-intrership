4a40756619b159ddfa9021f1b54769e6a4447fa0
i_zvyagin <zvyagin.i.gen@gmail.com>
2025-09-30 10:59:08 +0300

    Registered initial domain model

--- src/main/java/org/javaguru/travel/insurance/core/domain/Classifier.java
+++ src/main/java/org/javaguru/travel/insurance/core/domain/Classifier.java
@@ -1,4 +1,27 @@
 package org.javaguru.travel.insurance.core.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "classifiers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Classifier {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false, unique = true, length = 200)
+    private String title;
+
+    @Column(name = "description", nullable = false, unique = true, length = 100)
+    private String description;
 }
--- src/main/java/org/javaguru/travel/insurance/core/domain/ClassifierValue.java
+++ src/main/java/org/javaguru/travel/insurance/core/domain/ClassifierValue.java
@@ -1,4 +1,32 @@
 package org.javaguru.travel.insurance.core.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "classifier_values")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClassifierValue {
+
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "classifier_id", nullable = false)
+    private Classifier classifier;
+
+    @Column(name = "ic", nullable = false)
+    private String ic;
+
+    @Column(name = "description", nullable = false, unique = true, length = 500)
+    private String description;
 }
