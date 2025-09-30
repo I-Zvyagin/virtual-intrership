7a2897b7a45cfcf60b855bddf6dae42361272759
i_zvyagin <vanyazvyaginfr@gmail.com>
2025-09-30 16:04:16 +0300

    Replaced the tests with parameterized

--- src/test/java/org/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
+++ src/test/java/org/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
@@ -3,6 +3,8 @@ package org.javaguru.travel.insurance.core.repositories;
 import org.javaguru.travel.insurance.core.domain.ClassifierValue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -23,34 +25,20 @@ class ClassifierValueRepositoryTest {
         assertNotNull(classifierValueRepository);
     }
 
-    @Test
-    public void shouldFind_RiskType_TRAVEL_MEDICAL() {
-        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_MEDICAL");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_CANCELLATION() {
-        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_CANCELLATION");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_LOSS_BAGGAGE() {
-        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_LOSS_BAGGAGE");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_THIRD_PARTY_LIABILITY() {
-        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_THIRD_PARTY_LIABILITY");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_EVACUATION() {
-        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_EVACUATION");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_SPORT_ACTIVITIES() {
-        getClassifierValueAndAssert("RISK_TYPE", "TRAVEL_SPORT_ACTIVITIES");
+    @ParameterizedTest
+    @CsvSource({
+            "RISK_TYPE, TRAVEL_MEDICAL",
+            "RISK_TYPE, TRAVEL_CANCELLATION",
+            "RISK_TYPE, TRAVEL_LOSS_BAGGAGE",
+            "RISK_TYPE, TRAVEL_THIRD_PARTY_LIABILITY",
+            "RISK_TYPE, TRAVEL_EVACUATION",
+            "RISK_TYPE, TRAVEL_SPORT_ACTIVITIES"
+    })
+    public void getClassifierValueAndAssert(String classifierTitle, String ic) {    Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
+            classifierTitle, ic);
+        assertTrue(valueOpt.isPresent());
+        assertEquals(ic, valueOpt.get().getIc());
+        assertEquals(classifierTitle, valueOpt.get().getClassifier().getTitle());
     }
 
     @Test
@@ -59,12 +47,4 @@ class ClassifierValueRepositoryTest {
                 "RISK_TYPE", "FAKE");
         assertTrue(valueOpt.isEmpty());
     }
-
-    private void getClassifierValueAndAssert(String classifierTitle, String ic) {    Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-            classifierTitle, ic);
-        assertTrue(valueOpt.isPresent());
-        assertEquals(ic, valueOpt.get().getIc());
-        assertEquals(classifierTitle, valueOpt.get().getClassifier().getTitle());
-    }
-
 }
